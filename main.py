4f7d935c8af05c893c77e4cde864fe2011267a39
winfredselwyn <winfredselwyn@gmail.com>
2016-02-14 18:32:39 +0000

    added redo and undo function

--- main.py
+++ main.py
@@ -7,6 +7,34 @@ root = tk.Tk()
 root.geometry('800x600')
 root.title(PROGRAM_NAME)
 
+# basic text processing functions
+
+
+def undo():
+    content_text.event_generate('<<Undo>>')
+    return "break"
+
+
+def redo(event=None):
+    content_text.event_generate('<<Redo>>')
+    return "break"
+
+
+def cut():
+    content_text.event_generate('<<Cut>>')
+    return "break"
+
+
+def copy():
+    content_text.event_generate('<<Copy>>')
+    return "break"
+
+
+def paste():
+    content_text.event_generate('<<Paste>>')
+    return "break"
+
+
 # menu icons
 new_file_icon = tk.PhotoImage(file='icons/new_file.gif')
 open_file_icon = tk.PhotoImage(file='icons/open_file.gif')
@@ -36,16 +64,16 @@ file_menu.add_command(label='Exit', accelerator='Alt+F4')
 edit_menu = tk.Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label='Edit', menu=edit_menu)
 edit_menu.add_command(label='Undo', accelerator='Ctrl+Z',
-                      compound='left', image=undo_icon)
+                      compound='left', image=undo_icon, command=undo)
 edit_menu.add_command(label='Redo', accelerator='Ctrl+Y',
-                      compound='left', image=redo_icon)
+                      compound='left', image=redo_icon, command=redo)
 edit_menu.add_separator()
 edit_menu.add_command(label='Cut', accelerator='Ctrl+X',
-                      compound='left', image=cut_icon)
+                      compound='left', image=cut_icon, command=cut)
 edit_menu.add_command(label='Copy', accelerator='Ctrl+C',
-                      compound='left', image=copy_icon)
+                      compound='left', image=copy_icon, command=copy)
 edit_menu.add_command(label='Paste', accelerator='Ctrl+V',
-                      compound='left', image=paste_icon)
+                      compound='left', image=paste_icon, command=paste)
 edit_menu.add_separator()
 edit_menu.add_command(label='Find', underline=0, accelerator='Ctrl+F')
 edit_menu.add_separator()
@@ -102,12 +130,20 @@ line_number_bar = tk.Text(root, width=4, padx=3, takefocus=0, border=0,
 line_number_bar.pack(side='left', fill='y')
 
 
-content_text = tk.Text(root, wrap='word')
+content_text = tk.Text(root, wrap='word', undo=1)
 content_text.pack(expand='yes', fill='both')
 scroll_bar = tk.Scrollbar(content_text)
 content_text.configure(yscrollcommand=scroll_bar.set)
 scroll_bar.config(command=content_text.yview)
 scroll_bar.pack(side='right', fill='y')
+
+
+
+# bind redo to 'Ctrl + y'
+
+
+content_text.bind('<Control-y>', redo)
+content_text.bind('<Control-Y>', redo)
 
 
 root.mainloop()
